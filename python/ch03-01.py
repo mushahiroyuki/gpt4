3b0ca33bffcb4c284c91366f7cda0cdb28d65685
武舎広幸 (MUSHA, Hiroyuki) <mushahiroyuki@users.noreply.github.com>
2023-09-11 12:50:02 +0900

    update python version

--- python/ch03-01.py
+++ python/ch03-01.py
@@ -5,11 +5,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
-  messages=[],
+  messages=[
+    {
+      "role": "user",
+      "content": ""
+    }
+  ],
   temperature=1,
   max_tokens=256,
   top_p=1,
   frequency_penalty=0,
   presence_penalty=0
 )
-
--- python/ch03-01c.py
+++ python/ch03-01c.py
@@ -1,14 +1,15 @@
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY") # 環境変数からAPIのキーを取得
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {
       "role": "user",
-      "content": "情報の正確性という観点から次のツイートを分析してください。\nツイート: 50%以上の科学者は気候変動を信じていない。"
+      "content": "情報の正確性という観点から次のツイートを分析してください。\n" 
+        + "ツイート: 50%以上の科学者は気候変動を信じていない。"
     }
   ],
   temperature=1,
